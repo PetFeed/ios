9eca5985bf8f959f9ba158636a1081cf40e123f9
changhyeon743 <changhyeon743@gmail.com>
2018-09-09 19:33:02 +0900

    engineering crazy error

--- PetFeed/ViewControllers/NotificationVC.swift
+++ PetFeed/ViewControllers/NotificationVC.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class NotificationVC: UIViewController {
+class NotificationVC: UITableViewController {
 
+    @IBOutlet var myTableView: UITableView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        //myTableView.register(UINib(nibName: "NotificationCell", bundle: nil), forCellReuseIdentifier: "cell")
 
         // Do any additional setup after loading the view.
     }
@@ -20,6 +24,16 @@ class NotificationVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+//
+//        return cell!
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
     
     
 }
---
+++ PetFeed/Views/Cells/NotificationCell.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationCell.swift
+//  PetFeed
+//
+//  Created by 이창현 on 2018. 9. 9..
+//  Copyright © 2018년 이창현. All rights reserved.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var profileImageView: profileView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+}
